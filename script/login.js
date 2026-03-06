86e522342ba5a2d0324b5a86047cfcf80fa7a86c
kazimizan77 <kazimizan77@yahoo.com>
2026-03-06 21:00:32 +0600

    login page added

---
+++ script/login.js
@@ -0,0 +1,10 @@
+document.getElementById("btn-login").addEventListener("click", function () {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "admin123") {
+    window.location.href = "home.html";
+  } else {
+    alert("Invalid Username or Password");
+  }
+});
