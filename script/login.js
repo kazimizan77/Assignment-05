d8820a5332a218a75712183f9b1f36f5a5100aad
kazimizan77 <kazimizan77@yahoo.com>
2026-03-08 21:58:30 +0600

    feat: implement login page with basic authentication alert

--- script/login.js
+++ script/login.js
@@ -3,6 +3,7 @@ document.getElementById("btn-login").addEventListener("click", function () {
   const password = document.getElementById("password").value;
 
   if (username === "admin" && password === "admin123") {
+    alert('login successful')
     window.location.href = "home.html";
   } else {
     alert("Invalid Username or Password");
